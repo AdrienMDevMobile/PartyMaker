f3e182b0ccc9991baf5237d9a5954a7cb9246984
adriendev <privateadrienps@gmail.com>
2025-01-20 17:00:30 +0100

    Premiere compilation. il faut encore comprendre ce qu'il se passe.

---
+++ core/ui/src/main/kotlin/com/micheldr/partymaker/ButtonTest.kt
@@ -0,0 +1,12 @@
+package com.micheldr.partymaker
+
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ButtonTest() {
+    OutlinedButton (onClick = {}){
+        Text("Bonjour")
+    }
+}
